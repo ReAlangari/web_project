d75c6701d2f588ab58962f010e0d6080021eda78
fatimah777 <122534500+fatimah777@users.noreply.github.com>
2023-02-04 22:54:07 +0300

    Update in-patient-management1.js

--- web project/in-patient-management1.js
+++ web project/in-patient-management1.js
@@ -1,21 +1,41 @@
-
-function validateFOrm(){
-
-var a = document.forms["form"]["name"].value;	
-
-   count=0;
-   text="Please Fill ";
-    if (a == null || a == ""){
-	text+="Name, ";
-	count++;}
-
-	if (a.startsWith("0") || a.startsWith("1")|| a.startsWith("2")|| a.startsWith("3")|| a.startsWith("9")|| a.startsWith("4")|| a.startsWith("5")|| a.startsWith("6")|| a.startsWith("7")|| a.startsWith("8"))
-	{text+="*Name field cannot start with numbers*";
-    count++;}
-  
-	 if(count>0) {
-   alert(text+"Thank you");}	
-}
-
 function pictureRc() {
-alert("Name: Sara Khalid\nAdmission:2022/11/20\nDischarge Date:2023/1/1")}
+alert("Name: Sara Khalid\nAdmission:2022/11/20\nDischarge Date:2023/1/1")} 
+
+
+
+
+function validateform() {
+	count=0;
+	count1=0;
+	text="Please Fill ";
+	text1="";
+	var a = document.forms["form"]["name"].value;
+	var b=  document.forms["form"]["Date"].value;
+	date= new Date(b);
+	
+	if (a== null || a==""){
+		text+="Patient name, ";
+	count++;
+	}
+		if (b== null || b==""){
+		text+="Discharge Date, ";
+	count++;
+	}
+  if (a.startsWith("0") || a.startsWith("1")|| a.startsWith("2")|| a.startsWith("3")|| a.startsWith("9")|| a.startsWith("4")|| a.startsWith("5")|| a.startsWith("6")|| a.startsWith("7")|| a.startsWith("8"))
+	{text1+="\nName field cannot start with numbers";
+count1++;}
+
+if(date<new Date()){
+text1+="\nDate should be today's or after today's date";
+count1++;}
+
+	
+ if(count>0 || count1>0) {
+  if(count>0)
+  alert(text+"Thank you");
+  if(count1>0)
+	  alert(text1);
+  }
+}
+		
+		
