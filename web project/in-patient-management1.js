27ddc68ede0f229f3b2ee73392c738509618a8cd
fatimah777 <122534500+fatimah777@users.noreply.github.com>
2023-02-05 02:24:28 +0300

    Update in-patient-management1.js

--- web project/in-patient-management1.js
+++ web project/in-patient-management1.js
@@ -1,6 +1,45 @@
+/*function pictureRc() {
+alert("Name: Sara Khalid\nAdmission:2022/11/20\nDischarge Date:2023/1/1")}*/ 
+
+
+
+
+/*function validateform() {
+	count=0;
+	count1=0;
+	text="Please Fill ";
+	text1="";
+	var a = document.forms["form"]["name"].value;
+	var b=  document.forms["form"]["Date"].value;
+	date= new Date(b);
+	
+	if (a== null || a==""){
+		text+="Patient name, ";
+	count++;
+	}
+		if (b== null || b==""){
+		text+="Discharge Date, ";
+	count++;
+	}
+  if (a.startsWith("0") || a.startsWith("1")|| a.startsWith("2")|| a.startsWith("3")|| a.startsWith("9")|| a.startsWith("4")|| a.startsWith("5")|| a.startsWith("6")|| a.startsWith("7")|| a.startsWith("8"))
+	{text1+="\nName field cannot start with numbers";
+count1++;}
+
+if(date<new Date()){
+text1+="\nDate should be today's or after today's date";
+count1++;}
+
+	
+ if(count>0 || count1>0) {
+  if(count>0)
+  alert(text+"Thank you");
+  if(count1>0)
+	  alert(text1);
+  }
+}*/
+	
 const In_patient = document.querySelectorAll('.In-patient');
-console.log(In_patient
-)
+console.log(In_patient)
 
 In_patient.forEach((patient, i) => {
 
@@ -70,8 +109,8 @@ In_patient.forEach((patient, i) => {
               if(count>0)
              alert(text+"Thank you");
              if(count1>0)
-	         alert(text1);
-             
+	         alert(text1);}
+             if(count==0&&count1==0){
                 document.getElementById("form").innerHTML = `
                                 <div class="hidden">
                              
@@ -85,8 +124,9 @@ In_patient.forEach((patient, i) => {
                                 </br>
                                   </div>
                                 `
-                                patient.children[0].src="images/occupied-bed.png"
-           }
+								
+                                patient.children[0].src="images/occupied-bed.png"}
+           
           
 
         });
@@ -119,7 +159,6 @@ In_patient.forEach((patient, i) => {
 
     }
 
-
 });
 
 In_patient.forEach((patient, i) => {
@@ -133,4 +172,6 @@ In_patient.forEach((patient, i) => {
 
         
 
-})
+});
+	
+		
